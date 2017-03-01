3964113b32e6eef7feef90f1ed231cf4aed9f7c3
Tony Cannistra <tony.cannistra@gmail.com>
2017-03-01 08:56:30 -0800

    first setup commit, new redesign

--- gulpfile.js
+++
@@ -1,4 +0,0 @@
-var requireDir = require('require-dir');
-require('es6-promise').polyfill();
-// Require all tasks in gulp/tasks, including subfolders
-requireDir('./gulp/tasks', { recurse: true });
